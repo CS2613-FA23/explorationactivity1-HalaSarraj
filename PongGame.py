c280fca30356c069ffc6c8c67f603b49c7ffbae3
HalaSarraj <144267903+HalaSarraj@users.noreply.github.com>
2023-10-08 16:16:07 -0300

    Create PongGame.py
    Pong game. (Better run on a windows laptop, in a Linux machine the scoreboard is not displayed properly since height & width were defined based on my laptop / windows machine size)

---
+++ PongGame.py
@@ -0,0 +1,155 @@
+import turtle
+
+class PongGame:
+    def __init__(self):
+        self.screen = turtle.Screen()
+        self.screen.title('Pong Game')
+        self.screen.bgcolor('black')
+        self.screen.setup(width=1000, height=600)
+        self.left_paddle, self.right_paddle, self.ball, self.score_board = self.setup_game()
+        self.left_score = 0
+        self.right_score = 0
+
+    def setup_game(self):
+        left_paddle = turtle.Turtle()
+        left_paddle.speed(0)
+        left_paddle.shape('square')
+        left_paddle.color('red')
+        left_paddle.shapesize(stretch_wid=6, stretch_len=1)
+        left_paddle.penup()
+        left_paddle.goto(-400, 0)
+
+        right_paddle = turtle.Turtle()
+        right_paddle.speed(0)
+        right_paddle.shape('square')
+        right_paddle.color('blue')
+        right_paddle.shapesize(stretch_wid=6, stretch_len=1)
+        right_paddle.penup()
+        right_paddle.goto(400, 0)
+
+        ball = turtle.Turtle()
+        ball.speed(40)
+        ball.shape('circle')
+        ball.color('white')
+        ball.penup()
+        ball.goto(0, 0)
+        ball.dx = 5
+        ball.dy = -5
+
+        score_board = turtle.Turtle()
+        score_board.speed(0)
+        score_board.color('green')
+        score_board.penup()
+        score_board.hideturtle()
+        score_board.goto(0, 260)
+        score_board.write('Left Player: 0 \t Right Player: 0',
+                          align='center', font=('Times New Roman', 30, 'normal'))
+
+        # Display control instructions
+        control_instructions = turtle.Turtle()
+        control_instructions.speed(0)
+        control_instructions.color('white')
+        control_instructions.penup()
+        control_instructions.hideturtle()
+        control_instructions.goto(0, -280)
+        control_instructions.write("Controls:\nRight Player - Up: Up Arrow \tDown: Down Arrow\nLeft Player - Up: W Key\tDown: S Key\nFirst to 5 wins",
+                                   align='center', font=('Arial', 16, 'normal'))
+
+        return left_paddle, right_paddle, ball, score_board
+    
+    def left_paddle_up(self):
+        y = self.left_paddle.ycor()
+        if y < 240:
+            self.left_paddle.sety(y + 20)
+
+    def left_paddle_down(self):
+        y = self.left_paddle.ycor()
+        if y > -235:
+            self.left_paddle.sety(y - 20)
+
+    def right_paddle_up(self):
+        y = self.right_paddle.ycor()
+        if y < 240:
+            self.right_paddle.sety(y + 20)
+
+    def right_paddle_down(self):
+        y = self.right_paddle.ycor()
+        if y > -235:
+            self.right_paddle.sety(y - 20)
+
+    def keep_score(self, player):
+        if player == 'left':
+            self.left_score += 1
+        elif player == 'right':
+            self.right_score += 1
+
+        self.score_board.clear()
+        self.score_board.penup()
+        self.score_board.goto(0, 260)
+        self.score_board.write('Left Player: {} \t Right Player: {}'.format(
+            self.left_score, self.right_score), align='center', font=('Times New Roman', 30, 'normal'))
+
+    def run_game(self):
+        self.screen.listen()
+        self.screen.onkeypress(self.left_paddle_up, 'w')
+        self.screen.onkeypress(self.left_paddle_down, 's')
+        self.screen.onkeypress(self.right_paddle_up, 'Up')
+        self.screen.onkeypress(self.right_paddle_down, 'Down')
+
+        while True:
+            self.screen.update()
+            self.ball.setx(self.ball.xcor() + self.ball.dx)
+            self.ball.sety(self.ball.ycor() + self.ball.dy)
+
+            if self.ball.ycor() > 280:
+                self.ball.sety(280)
+                self.ball.dy *= -1
+
+            if self.ball.ycor() < -280:
+                self.ball.sety(-280)
+                self.ball.dy *= -1
+
+            if ((self.ball.xcor() > 360) and
+               (self.ball.xcor() < 370) and
+               (self.ball.ycor() < self.right_paddle.ycor()+40) and
+               (self.ball.ycor() > self.right_paddle.ycor()-40)):
+                self.ball.setx(360)
+                self.ball.dx *= -1
+
+            if ((self.ball.xcor() < -360) and
+               (self.ball.xcor() > -370) and
+               (self.ball.ycor() < self.left_paddle.ycor()+40) and
+               (self.ball.ycor() > self.left_paddle.ycor()-40)):
+                self.ball.setx(-360)
+                self.ball.dx *= -1
+
+            if self.ball.xcor() > 500:
+                self.ball.goto(0, 0)
+                self.ball.dx *= -1
+                self.keep_score('left')
+
+            elif self.ball.xcor() < -500:
+                self.ball.goto(0, 0)
+                self.ball.dx *= -1
+                self.keep_score('right')
+
+            if self.left_score >= 5 or self.right_score >= 5:
+                winner_message = turtle.Turtle()
+                winner_message.speed(0)
+                if self.left_score >= 5:
+                    winner_message.color('red')
+                    winner_message_message = "Left Player wins! Game Over"
+                else:
+                    winner_message.color('blue')
+                    winner_message_message = "Right Player wins! Game Over"
+                winner_message.penup()
+                winner_message.hideturtle()
+                winner_message.goto(0, 0)
+                winner_message.write(winner_message_message,
+                                    align='center', font=('Arial', 30, 'bold'))
+                self.screen.update()
+                turtle.done()
+
+if __name__ == '__main__':
+    game = PongGame()
+    game.run_game()
